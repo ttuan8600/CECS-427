3b4606139245c53b5b8e884fb848119a1775676a
Tuan M Tran <tranhoangminhtuan86@gmail.com>
2024-04-28 20:34:52 -0700

    ooo

--- Assignment5/demo/demo/spiders/testing.py
+++ Assignment5/demo/demo/spiders/testing.py
@@ -4,4 +4,11 @@ from networkx import draw, DiGraph, spring_layout
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
+
 class Spidey(CrawlSpider):
+    name = "mySpidey"
+    allowed_domains = ["dblg.org"]
+    start_urls = ["https://dblp.org/pid"]
+    rules = (
+        Rule(LinkExtractor(allow_domains=allowed_domains)),
+    )
